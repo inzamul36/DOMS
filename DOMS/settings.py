57259da15ac9e2ae080005fe2f19bda841ceb33d
Danilo Oliveira <danilooliveira@MacBook-Pro-de-Danilo.local>
2018-10-17 20:40:29 -0300

    estaticos

--- DOMS/settings.py
+++ DOMS/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/orders/static/'
 
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
