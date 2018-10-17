7e59cad7e3e5ba52e41c60d0e184c4d61e50319a
Danilo Oliveira <danilooliveira@MacBook-Pro-de-Danilo.local>
2018-10-17 20:10:00 -0300

    removendo status da aplicacao

--- DOMS/settings.py
+++ DOMS/settings.py
@@ -82,6 +82,17 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': '',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
