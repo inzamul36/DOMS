f041988d4a5501f31f0a2e5b2c17ae5364af0142
Danilo Oliveira <danilooliveira@MacBook-Pro-de-Danilo.local>
2018-10-17 17:44:02 -0300

    subindo a aplicacao sem o status

--- orders/forms.py
+++ orders/forms.py
@@ -4,20 +4,19 @@ from .models import Order
 
 class OrderForm(ModelForm):
     OPTIONS = (
-        ('',''),
-        ('Postpay','Postpay'),
-        ('Prepay (Full)','Prepay (Full)'),
-        ('Prepay (Half)', 'Prepay (Half)')
+        (0,'À vista - dinheiro'),
+        (1,'Débito'),
+        (2,'Crédito'),
     )
-    OPTIONS2 = (
-        ('Confirm', 'Confirm'),
-        ('Ready', 'Ready'),
-        ('Send', 'Send'),
-        ('Delivered', 'Delivered'),
-        ('Returned', 'Returned'),
-        ('Cancelled', 'Cancelled')
-    )
-    order_status = forms.TypedChoiceField(required=False, choices=OPTIONS2, widget=forms.RadioSelect)
+    # OPTIONS2 = (
+    #     (0, 'Confirmado'),
+    #     (1, 'Pronto'),
+    #     (2, 'Enviado'),
+    #     (3, 'Entregue'),
+    #     (4, 'Retornado'),
+    #     (5, 'Cancelado')
+    # )
+    #order_status = forms.TypedChoiceField(required=False, choices=OPTIONS2, widget=forms.RadioSelect)
     payment_option = forms.ChoiceField(choices=OPTIONS)
 
     class Meta:
