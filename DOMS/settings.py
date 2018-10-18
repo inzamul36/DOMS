47de6cd231c9e19a102bae17962a86c354ef878a
Danilo Oliveira <danilooliveira@MacBook-Pro-de-Danilo.local>
2018-10-17 22:26:44 -0300

    estaticos

--- DOMS/settings.py
+++ DOMS/settings.py
@@ -133,7 +133,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 # STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'orders/static'),
+#     os.path.join(BASE_DIR, '/static/'),
 # )
 
 LOGIN_REDIRECT_URL = '/'
