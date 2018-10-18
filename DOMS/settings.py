8c23eae63b40fd2fd06e71bf1e871c2466f3baf3
Danilo Oliveira <danilooliveira@MacBook-Pro-de-Danilo.local>
2018-10-17 22:14:01 -0300

    estaticos

--- DOMS/settings.py
+++ DOMS/settings.py
@@ -13,8 +13,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-ROOT_DIR = os.path.dirname(os.path.abspath('manage.py'))
-BASE_DIR = ROOT_DIR + '/DOMS/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -128,14 +127,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'orders/static'),
+# )
 
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'login'
