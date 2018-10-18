3ba41bfc03e7478314988ddb93bd91de2e969557
Danilo Oliveira <danilooliveira@MacBook-Pro-de-Danilo.local>
2018-10-17 21:12:45 -0300

    estaticos

--- DOMS/settings.py
+++ DOMS/settings.py
@@ -128,12 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = '/DOMS/static/'
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'login'
