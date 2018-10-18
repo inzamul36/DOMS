f534965cc353f419e04f4be611497a7af327c892
Danilo Oliveira <danilooliveira@MacBook-Pro-de-Danilo.local>
2018-10-17 21:27:12 -0300

    estaticos

--- DOMS/settings.py
+++ DOMS/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
